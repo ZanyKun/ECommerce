d3e23a4209ff77bc0a9da898e93a55d57b41dce3
jedidiahbowo <jedidiahbowo@yahoo.com>
2020-05-14 15:20:28 -0500

    add id to Product

--- src/main/java/com/b13/dto/Product.java
+++ src/main/java/com/b13/dto/Product.java
@@ -1,4 +1,10 @@
 package com.b13.dto;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "b13-products")
 public class Product {
+    @Id
+    long product_id;
 }
