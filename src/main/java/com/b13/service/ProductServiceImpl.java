0d2a7b7e2d11e04995bb18f6eb58ef828f58169e
jedidiahbowo <jedidiahbowo@yahoo.com>
2020-05-14 17:08:31 -0500

    commit service to pull

--- src/main/java/com/b13/service/ProductServiceImpl.java
+++ src/main/java/com/b13/service/ProductServiceImpl.java
@@ -1,13 +1,15 @@
 package com.b13.service;
 
+import com.b13.dao.ProductRepository;
 import com.b13.dto.Product;
 
 public class ProductServiceImpl implements ProductService {
 
+	ProductRepository repository;
+
 	@Override
 	public int createProduct(Product p) {
-		// TODO Auto-generated method stub
-		return 0;
+		return repository.save(p).getId();
 	}
 
 }
