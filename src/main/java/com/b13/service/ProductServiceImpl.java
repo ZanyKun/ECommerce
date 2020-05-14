088fd4d512a4d01d8763b781feba5423a03740b2
marlay365 <marlay365@gmail.com>
2020-05-14 16:28:18 -0500

    latest controller method

--- src/main/java/com/b13/service/ProductServiceImpl.java
+++ src/main/java/com/b13/service/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package com.b13.service;
 
+import java.util.Optional;
+
 import com.b13.dto.Product;
 
 public class ProductServiceImpl implements ProductService {
@@ -8,6 +10,12 @@ public class ProductServiceImpl implements ProductService {
 	public int createProduct(Product p) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public Optional<Product> getProductById(int id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
