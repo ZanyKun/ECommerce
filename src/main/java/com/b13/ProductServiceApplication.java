4beb47a85e2b8048bb959a4fc2b85e79b91c2872
ZanyKun <nunez.kev59@gmail.com>
2020-05-15 11:55:10 -0500

    Currently updated files

--- src/main/java/com/b13/ProductServiceApplication.java
+++ src/main/java/com/b13/ProductServiceApplication.java
@@ -3,6 +3,9 @@ package com.b13;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,9 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
